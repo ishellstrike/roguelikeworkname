76f85b8386c6679e53d86da605cce65c85065613
Hudson <hudson@mydomain.com>
2013-10-21 17:18:35 +0400

    autobuild test

--- rglikeworknamelib/AutoVersion.cs
+++ rglikeworknamelib/AutoVersion.cs
@@ -1,1 +1,1 @@
-namespace rglikeworknamelib{public static class AutoVersion{public static string Version = "v0.1.19-indev-1-g3511c98"; public static string Time = "1382314071";}}
+namespace rglikeworknamelib{public static class AutoVersion{public static string Version = "v0.1.19-indev-2-g5e56a16"; public static string Time = "1382357546";}}
