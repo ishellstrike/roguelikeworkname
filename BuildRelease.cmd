614aae407255de30612f3a6b08fcfd7c086fb401
ishellstrike <ishellstrike@gmail.com>
2013-12-09 18:56:16 +0300

    mapdata compression

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -4,5 +4,5 @@ rd rglikeworkname\rglikeworkname\bin\x86\Release /s /q
 del %VERSION%.tar
 del %VERSION%.tar.gz
 C:\windows\microsoft.net/framework/v4.0.30319/MSBuild.exe rglikeworkname.sln /p:Configuration=Release
-"C:\program files\7-zip\7z.exe" a -ttar %VERSION%.tar %CD%\rglikeworkname\rglikeworkname\bin\x86\Release\
+"C:\program files\7-zip\7z.exe" a -ttar %VERSION%.tar "%CD%\rglikeworkname\rglikeworkname\bin\x86\Release"
 "C:\program files\7-zip\7z.exe" a -tgzip %VERSION%.tar.gz %VERSION%.tar
