1564a1e62e469779b87e3bb5edae213b065a0c86
Andrey Samsonov <ishellstrike@gmail.com>
2014-02-16 17:30:20 +0300

    Update BuildRelease.cmd

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -1,4 +1,4 @@
-﻿#
+#
 for /F "tokens=*" %%i in ('git describe --tags HEAD') do set VERSION=%%i
 rd rglikeworkname\rglikeworkname\bin\x86\Release /s /q
 del %VERSION%.tar
@@ -6,7 +6,5 @@ del %VERSION%.tar.gz
 C:\windows\microsoft.net\framework\v4.0.30319\MSBuild.exe rglikeworkname.sln /p:Configuration=Release
 xcopy rglikeworkname\rglikeworknamecontent\Data rglikeworkname\rglikeworkname\bin\x86\Release\Content\Data\ /H /E /G /Q /R /Y
 xcopy rglikeworkname\rglikeworknamecontent\Textures rglikeworkname\rglikeworkname\bin\x86\Release\Content\Textures\ /H /E /G /Q /R /Y
-"C:\program files\7-zip\7z.exe" a -ttar %VERSION%.tar "%CD%\rglikeworkname\rglikeworkname\bin\x86\Release"
-"C:\program files\7-zip\7z.exe" a -tgzip %VERSION%.tar.gz %VERSION%.tar
-"C:\program files (x86)\7-zip\7z.exe" a -ttar %VERSION%.tar "%CD%\rglikeworkname\rglikeworkname\bin\x86\Release"
-"C:\program files (x86)\7-zip\7z.exe" a -tgzip %VERSION%.tar.gz %VERSION%.tar
+"C:\program files\7-zip\7z.exe" a -tzip -mx7 %VERSION%.zip "%CD%\rglikeworkname\rglikeworkname\bin\x86\Release"
+"C:\program files (x86)\7-zip\7z.exe" a -tzip -mx7 %VERSION%.zip "%CD%\rglikeworkname\rglikeworkname\bin\x86\Release"
