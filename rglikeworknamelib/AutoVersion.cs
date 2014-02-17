dffa3299a4000cb56369d9f9abd0bd5bdab1a63f
ishellstrike <ishellstrike@gmail.com>
2014-02-18 02:57:05 +0400

    +scheme

--- rglikeworknamelib/AutoVersion.cs
+++ rglikeworknamelib/AutoVersion.cs
@@ -1,1 +1,1 @@
-namespace rglikeworknamelib{public static class AutoVersion{public static string Version = "v0.1.19-indev-122-gd48843b"; public static string Time = "1392674654";}}
+namespace rglikeworknamelib{public static class AutoVersion{public static string Version = "v0.1.19-indev-123-g71fcdd5"; public static string Time = "1392677065";}}
