d48843b994fb664a5b5ec8ccfe2d6e54c2ad2fc7
Andrey Samsonov <ishellstrike@gmail.com>
2014-02-18 01:04:14 +0300

    Update BuildRelease.cmd

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -7,4 +7,3 @@ C:\windows\microsoft.net\framework\v4.0.30319\MSBuild.exe rglikeworkname.sln /p:
 xcopy rglikeworkname\rglikeworknamecontent\Data rglikeworkname\rglikeworkname\bin\x86\Release\Content\Data\ /H /E /G /Q /R /Y
 xcopy rglikeworkname\rglikeworknamecontent\Textures rglikeworkname\rglikeworkname\bin\x86\Release\Content\Textures\ /H /E /G /Q /R /Y
 "C:\program files\7-zip\7z.exe" a -tzip -mx7 %VERSION%.zip "%CD%\rglikeworkname\rglikeworkname\bin\x86\Release"
-"C:\program files (x86)\7-zip\7z.exe" a -tzip -mx7 %VERSION%.zip "%CD%\rglikeworkname\rglikeworkname\bin\x86\Release"
